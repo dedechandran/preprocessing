7c6fe5b3beb2512b546e3e765d7750051ba1bee9
dedechandran <dedechandra852.dc@gmail.com>
2021-01-02 19:37:21 +0700

    Add preprocess one sentence

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import prep
+
 app= Flask(__name__)
 
 @app.route('/')
 def index():
   return "<h1>Fatih Legend 4</h1>"
+
+@app.route('/preprocess-one-sentence',methods=['POST'])
+def preprocess_one_sentence():
+    req_data = request.get_json()
+    text = req_data['text']
+    preprocessed_text = prep.preprocess(text)
+    return {
+        "text": preprocessed_text
+    }
---
+++ app/preprocess.py
@@ -0,0 +1,52 @@
+from nltk.corpus import stopwords
+import re
+import string
+import qalsadi.lemmatizer
+
+punctuations = '''`÷×؛<>_()*&^%][ـ،/:"؟.,'{}~¦+|!”…“–ـ''' + string.punctuation
+
+# Arabic stop words with nltk
+stop_words = stopwords.words("arabic")
+stop_words.append("و")
+
+arabic_diacritics = re.compile("""
+                             ّ    | # Shadda
+                             َ    | # Fatha
+                             ً    | # Tanwin Fath
+                             ُ    | # Damma
+                             ٌ    | # Tanwin Damm
+                             ِ    | # Kasra
+                             ٍ    | # Tanwin Kasr
+                             ْ    | # Sukun
+                             ـ     # Tatwil/Kashida
+                         """, re.VERBOSE)
+
+def preprocess(text):
+    
+    '''
+    text is an arabic string input
+    
+    the preprocessed text is returned
+    '''
+    
+    #remove punctuations
+    translator = str.maketrans('', '', punctuations)
+    text = text.translate(translator)
+    
+    # remove Tashkeel
+    text = re.sub(arabic_diacritics, '', text)
+    
+    #remove longation
+    text = re.sub("[إأآا]", "ا", text)
+    text = re.sub("ى", "ي", text)
+    text = re.sub("ؤ", "ء", text)
+    text = re.sub("ئ", "ء", text)
+    text = re.sub("ة", "ه", text)
+    text = re.sub("گ", "ك", text)
+
+    text = ' '.join(word for word in text.split() if word not in stop_words)
+
+    lemmer = qalsadi.lemmatizer.Lemmatizer()
+    lemmetized = lemmer.lemmatize_text(text)
+
+    return lemmetized
